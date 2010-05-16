55a7a58597c696cec076058045cd42718b207aad
Shaun Ashby <sashby@dtmac.local>
2010-05-16 22:45:29 +0200

    Start to flesh out Service classes.

--- lib/Heavens/VO/Service.pm
+++ lib/Heavens/VO/Service.pm
@@ -2,6 +2,7 @@ package Heavens::VO::Service;
 
 use warnings;
 use strict;
+use Carp qw(croak);
 
 our $VERSION = '0.01';
 
@@ -11,6 +12,17 @@ sub new() {
     my $proto = shift;
     my $class = ref($proto) || $proto;
     return bless( {} => $class);
+}
+
+sub register_command() {
+    my ($self,$cmd,$args) = @_;
+    do {
+	use IPC::Cmd;
+	if (!IPC::Cmd::can_run($cmd)) {
+	    croak("Unable to find executable \"$cmd\"");
+	}
+    } if ($cmd && $args);
+
 }
 
 1;
---
+++ lib/Heavens/VO/Service/Cutout.pm
@@ -0,0 +1,29 @@
+#____________________________________________________________________ 
+# File: Cutout.pm
+#____________________________________________________________________ 
+#  
+# Author: Shaun Ashby <Shaun.Ashby@gmail.com>
+# Update: 2010-05-16 22:36:36+0200
+# Revision: $Id$ 
+#
+# Copyright: 2010 (C) Shaun Ashby
+#
+#--------------------------------------------------------------------
+package Heavens::VO::Service::Cutout;
+
+our @ISA = ('Heavens::VO::Service');
+
+use constant SERVICE_EXECUTABLE => 'wcs_pixels_params';
+
+sub new(){
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = bless({} => $class);
+    %self->SUPER::register_command(SERVICE_EXECUTABLE,"");
+    return $self;
+}
+
+
+1;
+
+__END__
