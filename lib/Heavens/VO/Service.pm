6210d0f7c04095d484acddfc7c1c6ea48eebe5d4
Shaun Ashby <Shaun.Ashby@unige.ch>
2010-05-17 16:50:10 +0200

    Template run method.

--- lib/Heavens/VO/Service.pm
+++ lib/Heavens/VO/Service.pm
@@ -25,4 +25,43 @@ sub register_command() {
 
 }
 
+sub run() {
+    my ($self, @args) = @_;
+    my $cmd = $self->bin_name;
+    my $full_path;
+    
+    if ( !( $full_path = IPC::Cmd::can_run($cmd) ) ) {
+	$self->stderr([ qq{couldn't find command '$cmd'} ]);
+	$self->status(255);
+	return;
+    }
+    
+    @args = $self->cmd_args( @args );
+
+    my ( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) =
+	IPC::Cmd::run( command => [ $full_path, @args ] );
+    
+    if ( !$success ) {
+	my ($err) = ($error_code =~ /.*?exited with value (.*?)$/); 
+	$self->status($err);
+	# All of the output buffers are references to arrays which contain the entire
+	# buffer as a single string in the first element. Take this and split it on
+	# newlines, then return a new array ref with each line appended with a newline:	
+	my $outbuf = [ map { $_."\n" } split(/\n/,$full_buf->[0] ) ];
+	# Write to STDERR buffer, scanning for usual ISDC error string:
+	$self->stderr([ grep { $_ =~ /Error/ } @$outbuf ]);
+	$self->stdout($outbuf);
+    } else {
+	# Turn off unitialized warnings: either that or we have to check whether in fact there
+	# is contents in the out/err buffers...some programs are broken and return only STDOUT,
+	# even when there are errors. Sillyness.
+	{
+	    no warnings 'uninitialized';
+	    # Return buffers with correct newlines (corrected array contents):
+	    $self->stdout([ map { $_."\n" } split(/\n/,$stdout_buf->[0] ) ]);
+	    $self->stderr([ map { $_."\n" } split(/\n/,$stderr_buf->[0] ) ]);
+	}
+    }
+}
+
 1;
