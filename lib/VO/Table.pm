c44611f66b42c2ff25b0d3b6436b1865f77eb7eb
Shaun Ashby <Shaun.Ashby@unige.ch>
2010-06-03 16:57:21 +0200

    Use TT for rendering of VO::Table object.

--- lib/VO/Table.pm
+++ lib/VO/Table.pm
@@ -10,15 +10,38 @@ use constant VO_QUERY_STATUS_OK       => 'OK';
 use constant VO_QUERY_STATUS_ERROR    => 'ERROR';
 use constant VO_QUERY_STATUS_OVERFLOW => 'OVERFLOW';
 
+use Template;
+
+use VO::Config qw(:config);
+
+use overload q{""} => \&render();
+
 sub new() {
     my $proto = shift;
     my $class = ref($proto) || $proto;
-    return bless(
-	{
-	    VO_QUERY_STATUS => VO_QUERY_STATUS_OK,
-	    VO_PARAMETERS   => {}
-	} => $class);
+    my $self = (@_ == 0) ? # Error if no params given
+	croak("No params given.\n")
+	: (ref($_[0]) eq 'HASH') ? shift
+	: croak("Needs a parameter hash ref");
+
+    $self->{VO_QUERY_STATUS} = VO_QUERY_STATUS_OK;
+    $self->{VO_PARAMETERS} = {};
+    
+    $self->{vodata} = {
+	voentries => $self->{voentries},
+    };
+    
+    # TT config:
+    $self->{tt} = Template->new(
+	config => {
+	    START_TAG => '[%',
+	    END_TAG => '%]',
+	    INCLUDE_PATH => $ENV{VO_TEMPLATE_PATH} || VO_TEMPLATE_PATH,
+	});
+    return bless($self,$class);
 }
+
+sub voentries { return shift->{entries}; }
 
 sub query_status() {
     my $self = shift;
@@ -28,7 +51,17 @@ sub query_status() {
 
 sub render() {
     my $self = shift;
-    
+}
+
+sub write() {
+    my $self = shift;
+    my $output = shift || '';
+    $self->{tt}->process(\*DATA, $self->{data},$output) || die $self->{tt}->error();
 }
 
 1;
+
+__DATA__
+[% FOREACH voentry IN voentries %]
+[% PROCESS voentry %]
+[% END %]
