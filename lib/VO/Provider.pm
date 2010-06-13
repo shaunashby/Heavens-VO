b30cf8d8e9f2305154794e846ef891a7b49d9c22
Shaun Ashby <Shaun.Ashby@gmail.com>
2010-06-13 18:41:13 +0200

    Split Provider::run method into 2 parts, one specific for MP2 environment.

--- lib/VO/Provider.pm
+++ lib/VO/Provider.pm
@@ -15,6 +15,12 @@ use strict;
 use warnings;
 
 use Carp qw(croak);
+use Config;
+
+use mod_perl2;
+use constant MP2 => (exists($ENV{MOD_PERL_API_VERSION}) and $ENV{MOD_PERL_API_VERSION} >= 2 );
+use constant PERLIO_IS_ENABLED => $Config{useperlio};
+
 use IPC::Cmd;
 
 sub new() {
@@ -27,11 +33,11 @@ sub register_exe() {
     my $self = shift;
     my $cmd = shift;
     my $fpath;
-    
+
     if ( !($fpath = IPC::Cmd::can_run($cmd)) ) {
 	croak(sprintf("Unable to find executable %-s",$cmd));
     }
-    
+
     $self->{provider_exe} = $fpath;
     $self->{provider_env} = shift || {};
 
@@ -45,6 +51,56 @@ sub stdout() { return shift->{stdout} || []; }
 sub stderr() { return shift->{stderr} || []; }
 
 sub run() {
+    my ($self, @args) = @_;
+    # If we're in a mod_perl2 environment then run using subprocess within Apache
+    # otherwise just use IPC::Cmd:
+    if (MP2) {
+	$self->subprocess_run(@args);
+    } else {
+	$self->ipc_run(@args);
+    }
+}
+
+sub subprocess_run() {
+    my ($self, @args) = @_;
+    my ($in_fh, $out_fh, $err_fh);
+    
+    use Apache2::RequestUtil;
+    use Apache2::SubProcess;
+    
+    # Retrieve the request object:
+    my $req = Apache2::RequestUtil->request;
+
+    # Set up the environment:
+    map {
+	$req->subprocess_env->set( $_ => $self->{provider_env}->{$_} );
+    } keys %{ $self->{provider_env} };
+    
+    # If there are args:
+    if ($#args) {
+	($in_fh, $out_fh, $err_fh) = $req->spawn_proc_prog($self->{provider_exe},[ @args ]);
+    } else {
+	($in_fh, $out_fh, $err_fh) = $req->spawn_proc_prog($self->{provider_exe});
+    }
+
+    # Read the output data:
+    my $data;
+    if (PERLIO_IS_ENABLED || IO::Select->new($out_fh)->can_read(10)) {
+	$data = [ <$out_fh> ];
+	$self->{stdout} = $data;
+	$self->{status} = 0;
+    }
+
+    # Read the error output:
+    if (PERLIO_IS_ENABLED || IO::Select->new($err_fh)->can_read(10)) {
+	$data = [ <$err_fh> ];
+	$self->{stderr} = $data;
+    }
+
+    $self->{status} = $#{$self->{stderr}} + 1;
+}
+
+sub ipc_run() {
     my ($self, @args) = @_;
 
     # Set up the environment:
